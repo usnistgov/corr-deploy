a095c30de8ce974b7be68e5104373652e77926ef
CONGO Faïçal Yannick Palingwendé <yannick.congo@gmail.com>
2018-02-12 14:52:03 -0500

    docs version number as string.

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ copyright = u'NIST MGI'
 # built documents.
 #
 # The short X.Y version.
-version = 0.1
+version = "0.1"
 # The full version, including alpha/beta/rc tags.
 release = "0.1"
 
@@ -140,7 +140,7 @@ html_favicon = 'logo.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
